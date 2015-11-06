9b00e16b8097fcc37414aed793ab6baa3e5cc941
PetroKiryshkin <petr.kiryshkin@testmatick.com>
2015-11-06 15:26:05 +0200

    Fix type org name method 2

--- src/com/cdpapp/pages/OrganizationSetup.java
+++ src/com/cdpapp/pages/OrganizationSetup.java
@@ -12,7 +12,7 @@ public class OrganizationSetup extends BasePage {
 
     public void setNameOrganization(){
         String nameOrg = "TestOrg" + Random.genString(4);
-        typeWithJS("Set organization name", nameOrg, "organizationNameField");
+        type("Set organization name", nameOrg, "organizationNameField");
     }
 
     public void selectStateOrProvinceOrganization(){
