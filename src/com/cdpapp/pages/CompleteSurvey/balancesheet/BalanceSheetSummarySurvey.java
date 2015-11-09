dbd04a78f50bf576a1e60998b3360e60ccbbce9a
PetroKiryshkin <petr.kiryshkin@testmatick.com>
2015-11-09 21:23:32 +0200

    Add "Balance Sheet: Net Assets" page

---
+++ src/com/cdpapp/pages/CompleteSurvey/balancesheet/BalanceSheetSummarySurvey.java
@@ -0,0 +1,7 @@
+package com.cdpapp.pages.completesurvey.balancesheet;
+
+/**
+ * Created by Petr on 09.11.2015.
+ */
+public class BalanceSheetSummarySurvey {
+}
---
+++ src/com/cdpapp/pages/CompleteSurvey/balancesheet/NetAssetsSurvey.java
@@ -0,0 +1,26 @@
+package com.cdpapp.pages.completesurvey.balancesheet;
+
+import com.qatestlab.base.BasePage;
+
+/**
+ * Created by Petr on 09.11.2015.
+ */
+public class NetAssetsSurvey extends BasePage {
+
+    public void setUnrestrictedNetAssets(){
+        type("Set Unrestricted Net Assets", "1", "otherNotesLoansNonCurrentField");
+    }
+
+    public void setTotalLiabilitiesAndNetAssets1(){
+        type("Set Total Liabilities and Net Assets 1", "1", "totalLiabilitiesNetAssetsField1");
+    }
+
+    public void setTotalLiabilitiesAndNetAssets2(){
+        type("Set Total Liabilities and Net Assets 2", "1", "totalLiabilitiesNetAssetsField2");
+    }
+
+    public void setTotalLiabilitiesAndNetAssets3(){
+        type("Set Total Liabilities and Net Assets 3", "1", "totalLiabilitiesNetAssetsField3");
+    }
+
+}
