fc661c38ba37d8d3053c46b967eff0525c15a2b0
PetroKiryshkin <petr.kiryshkin@testmatick.com>
2015-11-09 21:25:12 +0200

    Add "Balance Sheet" pages

--- src/com/cdpapp/pages/CompleteSurvey/balancesheet/BalanceSheetSummarySurvey.java
+++ src/com/cdpapp/pages/CompleteSurvey/balancesheet/BalanceSheetSummarySurvey.java
@@ -1,7 +1,18 @@
 package com.cdpapp.pages.completesurvey.balancesheet;
 
+import com.qatestlab.base.BasePage;
+
 /**
  * Created by Petr on 09.11.2015.
  */
-public class BalanceSheetSummarySurvey {
+public class BalanceSheetSummarySurvey extends BasePage{
+
+    public void setTotalAssets(){
+        type("Set Total Assets", "1", "totalAssetsField");
+    }
+
+    public void setTotalLiabilities(){
+        type("Set Total Liabilities", "1", "totalLiabilitiesField");
+    }
+
 }
