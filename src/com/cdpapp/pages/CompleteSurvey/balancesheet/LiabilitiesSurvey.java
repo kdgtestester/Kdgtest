abc9a3816726d30f6e119e4ec628d26ebd96c593
PetroKiryshkin <petr.kiryshkin@testmatick.com>
2015-11-09 21:15:32 +0200

    Add "Balance Sheet: Liabilities" page

---
+++ src/com/cdpapp/pages/CompleteSurvey/balancesheet/LiabilitiesSurvey.java
@@ -0,0 +1,142 @@
+package com.cdpapp.pages.completesurvey.balancesheet;
+
+import com.qatestlab.base.BasePage;
+
+/**
+ * Created by Petr on 09.11.2015.
+ */
+public class LiabilitiesSurvey extends BasePage {
+
+    public void setUnrestrictedAccountsPayable(){
+        type("Set Unrestricted Accounts Payable", "1", "unrestrictedAccountsPayable");
+    }
+
+    public void setTemporarilyRestrictedAccountsPayable(){
+        type("Set Temporarily Restricted Accounts Payable", "1", "temporarilyRestrictedAccountsPayable");
+    }
+
+    public void setPermanentlyRestrictedAccountsPayable(){
+        type("Set Permanently Restricted Accounts Payable", "1", "permanentlyRestrictedAccountsPayable");
+    }
+
+    public void setUnrestrictedAccruedExpenses(){
+        type("Set Unrestricted Accrued Expenses", "1", "unrestrictedAccruedExpenses");
+    }
+
+    public void setTemporarilyRestrictedAccruedExpenses(){
+        type("Set Temporarily Restricted Accrued Expenses", "1", "temporarilyRestrictedAccruedExpenses");
+    }
+
+    public void setPermanentlyRestrictedAccruedExpenses(){
+        type("Set Permanently Restricted Accounts Payable", "1", "permanentlyRestrictedAccruedExpenses");
+    }
+
+    public void setUnrestrictedDeferredRevenue(){
+        type("Set Unrestricted Deferred Revenue", "1", "unrestrictedDeferredRevenue");
+    }
+
+    public void setTemporarilyRestrictedDeferredRevenue(){
+        type("Set Temporarily Restricted Deferred Revenue", "1", "temporarilyRestrictedDeferredRevenue");
+    }
+
+    public void setPermanentlyRestrictedDeferredRevenue(){
+        type("Set Permanently Restricted Deferred Revenue", "1", "permanentlyRestrictedDeferredRevenue");
+    }
+
+    public void setUnrestrictedLoansCurrent(){
+        type("Set Unrestricted Loans - Current", "1", "unrestrictedLoansCurrent");
+    }
+
+    public void setTemporarilyRestrictedLoansCurrent(){
+        type("Set Temporarily Restricted Loans - Current", "1", "temporarilyRestrictedLoansCurrent");
+    }
+
+    public void setPermanentlyRestrictedLoansCurrent(){
+        type("Set Permanently Restricted Loans - Current", "1", "permanentlyRestrictedLoansCurrent");
+    }
+
+    public void setUnrestrictedOtherCurrentLiabilities(){
+        type("Set Unrestricted Other Current Liabilities", "1", "unrestrictedOtherCurrentLiabilities");
+    }
+
+    public void setTemporarilyRestrictedOtherCurrentLiabilities(){
+        type("Set Temporarily Restricted Other Current Liabilities", "1", "temporarilyRestrictedOtherCurrentLiabilities");
+    }
+
+    public void setPermanentlyRestrictedOtherCurrentLiabilities(){
+        type("Set Permanently Restricted Other Current Liabilities", "1", "permanentlyRestrictedOtherCurrentLiabilities");
+    }
+
+    public void setUnrestrictedTotalCurrentLiabilities(){
+        type("Set Unrestricted Total Current Liabilities", "1", "unrestrictedTotalCurrentLiabilities");
+    }
+
+    public void setTemporarilyRestrictedTotalCurrentLiabilities(){
+        type("Set Temporarily Restricted Total Current Liabilities", "1", "temporarilyRestrictedTotalCurrentLiabilities");
+    }
+
+    public void setPermanentlyRestrictedTotalCurrentLiabilities(){
+        type("Set Permanently Restricted Total Current Liabilities", "1", "permanentlyRestrictedTotalCurrentLiabilities");
+    }
+
+    public void setUnrestrictedLoansNonCurrent(){
+        type("Set Unrestricted Loans - Non-Current", "1", "unrestrictedLoansNonCurrent");
+    }
+
+    public void setTemporarilyRestrictedLoansNonCurrent(){
+        type("Set Temporarily Restricted Loans - Non-Current", "1", "temporarilyRestrictedLoansNonCurrent");
+    }
+
+    public void setPermanentlyRestrictedLoansNonCurrent(){
+        type("Set Permanently Restricted Loans - Non-Current", "1", "permanentlyRestrictedLoansNonCurrent");
+    }
+
+    public void setUnrestrictedOtherNonCurrentLiabilities(){
+        type("Set Unrestricted Other Non-Current Liabilities", "1", "unrestrictedOtherNonCurrentLiabilities");
+    }
+
+    public void setTemporarilyRestrictedOtherNonCurrentLiabilities(){
+        type("Set Temporarily Restricted Other Non-Current Liabilities", "1", "temporarilyRestrictedOtherNonCurrentLiabilities");
+    }
+
+    public void setPermanentlyRestrictedOtherNonCurrentLiabilities(){
+        type("Set Permanently Restricted Other Non-Current Liabilities", "1", "permanentlyRestrictedOtherNonCurrentLiabilities");
+    }
+
+    public void setUnrestrictedTotalNonCurrentLiabilities(){
+        type("Set Unrestricted Total Non-Current Liabilities", "1", "unrestrictedTotalNonCurrentLiabilities");
+    }
+
+    public void setTemporarilyRestrictedTotalNonCurrentLiabilities(){
+        type("Set Temporarily Restricted Total Non-Current Liabilities", "1", "temporarilyRestrictedTotalNonCurrentLiabilities");
+    }
+
+    public void setPermanentlyRestrictedTotalNonCurrentLiabilities(){
+        type("Set Permanently Restricted Total Non-Current Liabilities", "1", "permanentlyRestrictedTotalNonCurrentLiabilities");
+    }
+
+    public void setUnrestrictedDueToDueFrom(){
+        type("Set Unrestricted Due To/(Due From) ", "1", "unrestrictedDueToDueFromLiabilities");
+    }
+
+    public void setTemporarilyRestrictedDueToDueFrom(){
+        type("Set Temporarily Restricted Due To/(Due From) ", "1", "temporarilyRestrictedDueToDueFromLiabilities");
+    }
+
+    public void setPermanentlyRestrictedDueToDueFrom(){
+        type("Set Permanently Restricted Due To/(Due From) ", "1", "permanentlyRestrictedTotalDueToDueFromLiabilities");
+    }
+
+    public void setUnrestrictedTotalLiabilities(){
+        type("Set Unrestricted Total Liabilities", "1", "unrestrictedTotalLiabilities");
+    }
+
+    public void setTemporarilyRestrictedTotalLiabilities(){
+        type("Set Temporarily Restricted Total Liabilities", "1", "temporarilyRestrictedTotalLiabilities");
+    }
+
+    public void setPermanentlyRestrictedTotalLiabilities(){
+        type("Set Permanently Restricted Total Liabilities", "1", "permanentlyRestrictedTotalLiabilities");
+    }
+
+}
---
+++ src/com/cdpapp/pages/CompleteSurvey/balancesheet/LoanDetailsSurvey.java
@@ -0,0 +1,9 @@
+package com.cdpapp.pages.completesurvey.balancesheet;
+
+import com.qatestlab.base.BasePage;
+
+/**
+ * Created by Petr on 09.11.2015.
+ */
+public class LoanDetailsSurvey extends BasePage {
+}
