e55a8dd3f8e1918f6712d67fa439443676611e9b
PetroKiryshkin <petr.kiryshkin@testmatick.com>
2015-11-09 21:46:52 +0200

    Add "Program Activity: Advocacy" page

---
+++ src/com/cdpapp/pages/CompleteSurvey/programactivity/AdvocacySurvey.java
@@ -0,0 +1,26 @@
+package com.cdpapp.pages.completesurvey.programactivity;
+
+import com.qatestlab.base.BasePage;
+
+/**
+ * Created by Petr on 09.11.2015.
+ */
+public class AdvocacySurvey extends BasePage {
+
+    public void setAdvocacyDescription(){
+        type("Set Advocacy Description", "1", "advocacyDescriptionArea");
+    }
+
+    public void setMeetings(){
+        type("Set Meetings", "1", "meetingsField");
+    }
+
+    public void setOfficials(){
+        type("Set Officials", "1", "officialsField");
+    }
+
+    public void setTestimony(){
+        type("Set Testimony", "1", "testimonyField");
+    }
+
+}
